a2987a3e9d7a526b4911b58cb4e43f3c7a65af34
dimon <dimon@SERMA>
2022-03-11 14:37:19 +0300

    Добавьте файлы проекта.

---
+++ Zadanie2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab2
+{
+
+    public class Detal
+    {
+
+        int var1;
+        string var2;
+        string var3;
+        public void Func1() { }
+        public void Func2() { }
+        public virtual void Func3() { }
+
+    }
+
+    public class Mechanism : Detal
+    {
+        int var1;
+        string var2;
+        string var3;
+        public void Func1() { }
+        public void Func2() { }
+        public override void Func3() { }
+
+    }
+    public class Izdelie : Mechanism
+    {
+        int var1;
+        string var2;
+        string var3;
+        public void Func1() { }
+        public void Func2() { }
+        public void Func3() { }
+
+    }
+    public class Uzel : Izdelie
+    {
+        int var1;
+        string var2;
+        string var3;
+        public void Func1() { }
+        public void Func2() { }
+        public void Func3() { }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
